66d17baa06cec44083811a2578925cdfb3dfe7c8
Saurav Garg <github@softradix.in>
2021-10-14 12:22:22 +0530

    call kit functionality

---
+++ CallKitTutorial/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  CallKitTutorial
+//
+//  Created by apple on 27/08/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+    
+}
+
---
+++ CallerData/CallerData.h
@@ -0,0 +1,20 @@
+//
+//  CallerData.h
+//  CallerData
+//
+//
+//  Created by apple on 27/08/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CallerData.
+FOUNDATION_EXPORT double CallerDataVersionNumber;
+
+//! Project version string for CallerData.
+FOUNDATION_EXPORT const unsigned char CallerDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CallerData/PublicHeader.h>
+
+
